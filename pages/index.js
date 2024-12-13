930143804c562bb3d8009be790d1b57073768e67
Pedro Soares <pscoutosoares@gmail.com>
2024-12-13 14:27:02 +0000

    chore: update test page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
 function Home() {
-    return <h1>Teste</h1>
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      const query = await fetch("https://api.thedogapi.com/v1/images/search");
+      const response = await query.json();
+      setImage(response[0]);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Doguito aleatório</h1>
+      {image && <img src={image.url} width={image.width} />}
+    </div>
+  );
 }
 
 export default Home;
