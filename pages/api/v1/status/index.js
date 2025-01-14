d9ca3250d4d85418dd6259100f510e852d947ec2
Pedro Soares <pscoutosoares@gmail.com>
2025-01-14 21:21:30 +0000

    chore: update status endpoint with db info

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,16 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "teste" });
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  const currentConnections = await database.query(
+    "SELECT sum(numbackends) FROM pg_stat_database;",
+  );
+  response.status(200).json({
+    updated_at: updatedAt,
+    max_connections: maxConnections.rows[0].max_connections,
+    current_connections: currentConnections.rows[0].sum,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const updatedAtParsedToIso = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(updatedAtParsedToIso);
+
+  const max_connections = parseInt(responseBody.max_connections);
+  const current_connections = parseInt(responseBody.current_connections);
+  expect(max_connections).toBeGreaterThan(0);
+  expect(current_connections).toBeGreaterThan(0);
+  expect(current_connections).toBeLessThanOrEqual(max_connections);
 });
