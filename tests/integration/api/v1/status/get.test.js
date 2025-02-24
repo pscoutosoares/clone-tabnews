8b9d14b7cab1c012ef22741e2f5f8d6a2486a68c
Pedro Soares <pscoutosoares@gmail.com>
2025-02-24 15:53:31 -0300

    chore: improve test description and quality

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,23 +4,29 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Annonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const updatedAtParsedToIso = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(updatedAtParsedToIso);
+      const updatedAtParsedToIso = new Date(
+        responseBody.updated_at,
+      ).toISOString();
+      expect(responseBody.updated_at).toEqual(updatedAtParsedToIso);
 
-  const max_connections = parseInt(
-    responseBody.dependencies.database.max_connections,
-  );
-  const opened_connections = parseInt(
-    responseBody.dependencies.database.opened_connections,
-  );
-  expect(max_connections).toBeGreaterThan(0);
-  expect(opened_connections).toBe(1);
-  expect(responseBody.dependencies.database.version).toBe("16.0");
+      const max_connections = parseInt(
+        responseBody.dependencies.database.max_connections,
+      );
+      const opened_connections = parseInt(
+        responseBody.dependencies.database.opened_connections,
+      );
+      expect(max_connections).toBeGreaterThan(0);
+      expect(opened_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+    });
+  });
 });
