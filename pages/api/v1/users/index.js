ea87530f1eeb9f959edf6122821f50323dd4c81d
Pedro Soares <pscoutosoares@gmail.com>
2025-06-01 18:29:58 -0300

    feat: add create user feature

---
+++ pages/api/v1/users/index.js
@@ -0,0 +1,14 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import user from "models/user.js";
+
+const router = createRouter();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const newUser = await user.create(request.body);
+  return response.status(201).json(newUser);
+}
---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,118 @@
+import orchestrator from "tests/orchestrator.js";
+import { version as uuidVersion } from "uuid";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runMigrations();
+});
+
+const ENDPOINT_URL = "http://localhost:3000/api/v1/users";
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("Check if the request field is valid", async () => {
+      const response = await fetch(ENDPOINT_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "pedroSoares",
+          email: "pedro@gmail.com",
+          password: "senha123",
+        }),
+      });
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "pedroSoares",
+        email: "pedro@gmail.com",
+        password: "senha123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("Check for duplicated emails", async () => {
+      const response1 = await fetch(ENDPOINT_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedUsername1",
+          email: "duplicated@gmail.com",
+          password: "senha123",
+        }),
+      });
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch(ENDPOINT_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedUsername2",
+          email: "duplicated@gmail.com",
+          password: "senha123",
+        }),
+      });
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+        status_code: 400,
+      });
+    });
+
+    test("Check for duplicated usernames", async () => {
+      const response1 = await fetch(ENDPOINT_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedUsername",
+          email: "duplicatedMail1@gmail.com",
+          password: "senha123",
+        }),
+      });
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch(ENDPOINT_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedUsername",
+          email: "duplicatedMail2@gmail.com",
+          password: "senha123",
+        }),
+      });
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+        status_code: 400,
+      });
+    });
+  });
+});
