ddcd580e7d9aa53dbe4f4f5d6a22c6ecc802881e
Pedro Soares <pscoutosoares@gmail.com>
2025-06-01 14:17:23 -0300

    feat: create migrations model and refactor controller

---
+++ models/migrator.js
@@ -0,0 +1,50 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+const migrationDefaultConfig = {
+  direction: "up",
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...migrationDefaultConfig,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...migrationDefaultConfig,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
