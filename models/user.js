d93ac2e377ad5f3e201e83803e28bce3fd2aa3e5
Pedro Soares <pscoutosoares@gmail.com>
2025-06-02 19:44:48 -0300

    feat: adds get user router

--- models/user.js
+++ models/user.js
@@ -1,5 +1,29 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const results = await database.query({
+    text: `
+    SELECT 
+      *
+    FROM 
+      users
+    WHERE 
+      LOWER(username) = LOWER($1)
+    LIMIT
+      1
+    ;`,
+    values: [username],
+  });
+  if (results.rowCount === 0) {
+    throw new NotFoundError({
+      message: "O username informado não foi encontrado no sistema.",
+      action: "Verifique se o username está digitado corretamente.",
+    });
+  }
+  return results.rows[0];
+}
+
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUsername(userInputValues.username);
@@ -47,7 +71,6 @@ async function create(userInputValues) {
       });
     }
   }
-
   async function runInsertQuery(userInputValues) {
     const result = await database.query({
       text: "INSERT INTO users (username, email, password) VALUES($1, $2, $3)  RETURNING *;",
@@ -63,6 +86,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
